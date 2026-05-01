a241c5d9cfbd7ec87370ae56375416a95a153e16
omertemo <omer_temo@hotmail.com>
2026-05-01 20:45:03 +0300

    before vercel last commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   images: {
-    domains: [],
+    unoptimized: true, // static export'ta Image optimization çalışmaz
   },
 };
-
 module.exports = nextConfig;
